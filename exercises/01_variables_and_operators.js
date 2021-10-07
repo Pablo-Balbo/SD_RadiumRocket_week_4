4d8e75450c5300e952c437de89b8127661e578de
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-07 10:45:34 -0300

    Resolution of exercise 1-B

--- exercises/01_variables_and_operators.js
+++ exercises/01_variables_and_operators.js
@@ -6,3 +6,13 @@ let b = 6;
 let c = a + b;
 
 console.log('4 + 6 = ' + c);
+
+// Create two variables of type String and concatenate them by saving the result in a 3rd variable.
+
+let firstString = 'Exit light, ';
+let secondString = 'enter night';
+
+let completeString = firstString + secondString;
+
+console.log(completeString + '!!!');
+
