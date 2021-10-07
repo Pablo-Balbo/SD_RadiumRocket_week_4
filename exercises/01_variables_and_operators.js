2d2302af6bc2accfc63314e716d22eb10a53ce3e
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-07 10:37:11 -0300

    Resolution of exercise 1-A

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,8 @@
+// Create two numeric variables and use the sum operator to save the value of the sum of both numbers in a 3rd variable.
+
+let a = 4;
+let b = 6;
+
+let c = a + b;
+
+console.log('4 + 6 = ' + c);
