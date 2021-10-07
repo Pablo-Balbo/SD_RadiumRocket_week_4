935bdee467ba23c86c172a621caf24dfc1b69977
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-07 11:06:58 -0300

    Resolution of exercise 2-A

---
+++ exercises/02_strings.js
@@ -0,0 +1,23 @@
+// Create a string variable with at least 10 characters and convert all text to uppercase (use toUpperCase).
+
+let longString = 'welcome to the Radium Rocket Software Developer course';
+
+console.log(longString.toUpperCase());
+
+// Create a variable of type string with at least 10 characters and generate a new string with the first 5 characters, saving the result in a new variable (use substring).
+
+
+
+// Create a variable of type string with at least 10 characters and generate a new string with the last 3 characters, saving the result in a new variable (use substring).
+
+
+
+// Create a variable of type string with at least 10 characters and generate a new string with the first letter in uppercase and the others in lowercase. Save the result in a new variable (use substring, toUpperCase, toLowerCase, and the + operator).
+
+
+
+// Create a variable of type string with at least 10 characters and some blank space. Find the position of the first whitespace and save it to a variable (use indexOf).
+
+
+
+// Create a string variable with at least 2 long words (10 characters and some space in between). Use the methods of the previous exercises to generate a new string that has the first letter of both words in uppercase and the other letters in lowercase (use indexOf, substring, toUpperCase, toLowerCase and the operator +).
