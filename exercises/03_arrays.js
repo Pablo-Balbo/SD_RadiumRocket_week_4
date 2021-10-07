6ddd6e0cf6a8693a0a8205e3b7efac1771839644
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-07 16:45:09 -0300

    Resolution of exercise 3-A

---
+++ exercises/03_arrays.js
@@ -0,0 +1,27 @@
+// Given the following array: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] show months 5 and 11 per console (use console.log).
+
+const array = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+console.log(array[4] + ' - ' + array[10]);
+
+// Sort the array of months alphabetically and display it by console (use sort).
+
+
+
+// Add an element to the beginning and end of the array (use unshift and push).
+
+
+
+// Remove an element from the beginning and the end of the array (use shift and pop).
+
+
+
+// Reverse the order of the array (use reverse).
+
+
+
+// Join all the elements of the array in a single string where each month is separated by a hyphen - (use join).
+
+
+
+// Create a copy of the array of months that contains from May to November (use slice).
