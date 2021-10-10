91db6c99dc7d8d54239265df702971612ef2bd68
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-10 20:14:06 -0300

    Resolution of exercise 3-G

--- exercises/03_arrays.js
+++ exercises/03_arrays.js
@@ -6,7 +6,7 @@ console.log(array[4] + ' - ' + array[10]);
 
 // Sort the array of months alphabetically and display it by console (use sort).
 
-console.log(array.sort());
+// console.log(array.sort());
 
 // Add an element to the beginning and end of the array (use unshift and push).
 
@@ -22,11 +22,13 @@ console.log(array);
 
 // Reverse the order of the array (use reverse).
 
-array.reverse();
-console.log(array);
+// array.reverse();
+// console.log(array);
 
 // Join all the elements of the array in a single string where each month is separated by a hyphen - (use join).
 
 console.log(array.join(' - '));
 
 // Create a copy of the array of months that contains from May to November (use slice).
+
+console.log(array.slice(4, 11));
