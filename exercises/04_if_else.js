f774671b80f8fb2be022f1a52c3a8a71fd0011f1
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-10 20:48:38 -0300

    Resolution of exercise 5-A

--- exercises/04_if_else.js
+++ exercises/04_if_else.js
@@ -1,13 +1,13 @@
 // Create a random number between 0 and 1 using the Math.random () function, if the value is greater than or equal to 0.5 show an alert with the message “Greater than 0.5” and if not an alert with the message “Lower than 0.5 ".
 
-let number = Math.random();
-if(number >= 0.5){
-    alert('Greater than 0.5')
-} else {
-    alert('Lower than 0.5')
-};
+// let number = Math.random();
+// if(number >= 0.5){
+//     alert('Greater than 0.5')
+// } else {
+//     alert('Lower than 0.5')
+// };
 
-console.log(number);
+// console.log(number);
 
 // Create an "Age" variable that contains an integer between 0 and 100 and displays the following alert messages:
     // - "Baby" if the age is less than 2 years;
@@ -18,20 +18,20 @@ console.log(number);
     // - “Senior adult” between 61 and 75 years old;
     // - "Elderly" if you are older than 75 years.
 
-let age = Math.random() * (101 - 0) + 0;
+// let age = Math.random() * (101 - 0) + 0;
 
-if(age < 2){
-    alert('Baby')
-} else if(age < 2 && age < 12){
-    alert('Child')
-} else if(age < 13 && age < 19){
-    alert('Adolescent')
-} else if(age < 20 && age < 30){
-    alert('Young')
-} else if(age < 31 && age < 60){
-    alert('Adult')
-} else if(age < 61 && age < 75){
-    alert('Senior adult')
-} else if(age < 75){
-    alert('Elderly')
-};
+// if(age < 2){
+//     alert('Baby')
+// } else if(age < 2 && age < 12){
+//     alert('Child')
+// } else if(age < 13 && age < 19){
+//     alert('Adolescent')
+// } else if(age < 20 && age < 30){
+//     alert('Young')
+// } else if(age < 31 && age < 60){
+//     alert('Adult')
+// } else if(age < 61 && age < 75){
+//     alert('Senior adult')
+// } else if(age < 75){
+//     alert('Elderly')
+// };
