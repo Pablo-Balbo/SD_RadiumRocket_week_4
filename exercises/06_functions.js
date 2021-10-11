a1c7067e206865e9de376ab0a980c0dc6907ec35
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-11 17:56:18 -0300

    Optimize function 6-B

--- exercises/06_functions.js
+++ exercises/06_functions.js
@@ -1,20 +1,40 @@
 // Create a sum function that receives two numeric values ​​and returns the result. Execute the function and save the result in a variable, showing the value of said variable in the browser console.
 
-function sumFunction(num1, num2) {
-    result = num1 + num2;
+function mySum(num1, num2) {
+    let result = num1 + num2;
     return result;
 }
 
-let sumResult = sumFunction(6, 8);
+let sumResult = mySum(6, 8);
 console.log(sumResult);
 
 // To the previous sum function, add a validation to control if any of the parameters is not a number, show an alert clarifying that one of the parameters has an error and return the NaN value as a result.
 
-// if(isNaN(num1)){
-//    alert('One of the parameters has an error');
-// } else if(isNaN(num2)){
-//     alert('One of the parameters has an error');
-// } 
+function mySumWithValidation(num1, num2) {
+    if(isNaN(num1)) {
+        alert('First value is not a number');
+        return num1;
+    } else if(isNaN(num2)) {
+        alert('Second value is not a number');
+        return num2;
+    }
+    let result = num1 + num2;
+    return result;
+}
+
+// ---------------Tests---------------
+
+let mySumWithValidationTest1 = mySumWithValidation('House', 6);
+console.log(mySumWithValidationTest1);
+
+let mySumWithValidationTest2 = mySumWithValidation(10 , 'Dog');
+console.log(mySumWithValidationTest2);
+
+let mySumWithValidationTest3 = mySumWithValidation('Tree', 'Cat');
+console.log(mySumWithValidationTest3);
+
+let mySumWithValidationTest4 = mySumWithValidation(15, 40);
+console.log(mySumWithValidationTest4);
 
 // Create a validate integer function that takes a number as a parameter and returns true if it is an integer.
 
