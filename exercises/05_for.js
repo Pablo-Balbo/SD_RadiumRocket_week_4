1c0283e0ded2109836209a1f82ca16d72683fce8
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-10 21:25:47 -0300

    Resolution of exercise 5-B

---
+++ exercises/05_for.js
@@ -0,0 +1,22 @@
+// Create an array containing 5 words and loop through the array using a JavaScript for loop to display an alert using each word.
+
+let forArray = ['football', 'basquetball', 'tennis', 'swimming', 'judo'];
+
+// for(i = 0; i < 5; i++){
+//     alert(forArray[i]);
+// }
+
+// To the above array, convert the first letter of each word to uppercase and show an alert for each modified word.
+
+for(i = 0; i < 5; i++){
+    let capitalizeArray = forArray[i].charAt(0).toUpperCase() + forArray[i].slice(1);
+    forArray[i] = capitalizeArray;
+
+    alert(forArray[i]);
+}
+
+// Create a variable called "sentence" that has an empty string, then to the array of point a), go through it with a for loop to save each word inside the sentence variable. At the end show a single alert with the complete chain.
+
+
+
+// Create an empty array with a for loop of 10 repetitions. Fill the array with the repetition number, that is, at the end of the execution of the for loop there should be 10 elements within the array, from number 0 to number 9. Show the final array through the browser console (use console .log).
