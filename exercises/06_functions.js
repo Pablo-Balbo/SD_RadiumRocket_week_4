95d0ebd2d34a79dfe98512fc66004c193911e1a6
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-11 16:40:46 -0300

    Resolution of exercise 6-A

---
+++ exercises/06_functions.js
@@ -0,0 +1,23 @@
+// Create a sum function that receives two numeric values ​​and returns the result. Execute the function and save the result in a variable, showing the value of said variable in the browser console.
+
+function sumFunction() {
+    result = 6 + 8;
+    return result;
+}
+
+let sumResult = sumFunction();
+console.log(sumResult);
+
+// To the previous sum function, add a validation to control if any of the parameters is not a number, show an alert clarifying that one of the parameters has an error and return the NaN value as a result.
+
+
+
+// Create a validate integer function that takes a number as a parameter and returns true if it is an integer.
+
+
+
+// To the sum function of exercise 6b) add a call that validates that the numbers are integers. In case there are decimals, show an alert with the error and return the number converted to integer (rounded).
+
+
+
+// Convert the validation of exercise 6b) into a separate function and call it within the sum function, testing that everything continues to work the same.
