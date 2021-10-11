6316fffc99187224b71f8b20a6b3733710fd6dcb
Pablo Balbo <pablobalbo6@gmail.com>
2021-10-11 15:57:47 -0300

    Correct exercise 3-G

--- exercises/03_arrays.js
+++ exercises/03_arrays.js
@@ -6,7 +6,7 @@ console.log(array[4] + ' - ' + array[10]);
 
 // Sort the array of months alphabetically and display it by console (use sort).
 
-// console.log(array.sort());
+console.log(array.sort());
 
 // Add an element to the beginning and end of the array (use unshift and push).
 
@@ -16,14 +16,14 @@ console.log(array);
 
 // Remove an element from the beginning and the end of the array (use shift and pop).
 
-array.shift('Turquía');
-array.pop(12);
+array.shift();
+array.pop();
 console.log(array);
 
 // Reverse the order of the array (use reverse).
 
-// array.reverse();
-// console.log(array);
+array.reverse();
+console.log(array);
 
 // Join all the elements of the array in a single string where each month is separated by a hyphen - (use join).
 
@@ -31,4 +31,7 @@ console.log(array.join(' - '));
 
 // Create a copy of the array of months that contains from May to November (use slice).
 
-console.log(array.slice(4, 11));
+const arrayCopy = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let newArray = arrayCopy.slice(4,11);
+console.log(newArray);
